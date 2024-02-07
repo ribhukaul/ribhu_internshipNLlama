6249425e29839359cdb6a531c8b8ca6c239fcec2
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-02-07 13:12:41 +0100

    lambda_function w real extractions

--- lambda_function.py
+++ lambda_function.py
@@ -4,26 +4,32 @@ from RequestContext import RequestContext
 from ExtractionHandler import ExtractionHandler
 from AWSInteraction.EnvVarSetter import EnvVarSetter
 
+""" For local testing needs to add the following environment variables to the config file
+at AWSInteraction\config.json:
+    - "S3_BUCKET": "dde-dev-working-671256662822-eu-south-1"
+    - "SECRET_MANAGER_SUFFIX": "-llm-secret
+    - "SECRET_MANAGER_PREFIX": "dde/dev/"
+    - "REGION": "eu-south-1"
+    - "AWS_PROFILE": "appicativo" (yoour profile name, to create your SSO profile follow 
+    https://docs.aws.amazon.com/cli/latest/userguide/sso-configure-profile-token.html#sso-configure-profile-token-auto-sso)
+    - "ENV":"local"
+"""
 
 def lambda_handler(event, context):
-    ####Temporary mockup
-    # # # # # Handle context
-    # # # requestContext = RequestContext(event)
 
-    # # # env_setter = EnvVarSetter(requestContext.payload)
-    # # # env_setter.set_locally_saved_env_vars()
-    # # # # tWE CAN ONLY TEST THIS IN DEV ENV CAUS OF THE SECRETS
-    # # # # env_setter.set_all_env_vars()
+    requestContext = RequestContext(event)
 
-    # # # #try:
-    # # # extraction_handler = ExtractionHandler(requestContext)
-    # # # extraction_handler.run()
-    # # # extracted_data = extraction_handler.extractions
-    # # # extracted_data_json = json.dumps(extracted_data)
-    # Load mock_output.json
+    env_setter = EnvVarSetter(requestContext.payload)
+    env_setter.set_all_env_vars()
+
     try:
-        with open('mock_output.json', 'r') as f:
-            extracted_data_json = f.read()
+        extraction_handler = ExtractionHandler(requestContext)
+        extraction_handler.run()
+        extracted_data = extraction_handler.extractions
+        extracted_data_json = json.dumps(extracted_data)
+    ## MOCKUP OUTPUT
+    #     with open('mock_output.json', 'r') as f:
+    #         extracted_data_json = f.read()
         return {
             'statusCode': 200,
             'extraction': extracted_data_json
@@ -37,8 +43,9 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
-    event = {"body": "{\r\n  \"files\": [\"allianz.pdf\"], \"TENANT\": \"insurance\", \"extractor_type\": \"kid\"\r\n}"}
+    import os
+    os.environ['ENV'] = 'local'
+    event = {"body": "{\r\n  \"files\": [\"basepfts/workspaces/JACOPO/202212_CNP Cross Life.pdf\"], \"TENANT\": \"insurance\", \"extractor_type\": \"kid\"\r\n}"}
     x = lambda_handler(event, None)
-    
-    print(3)
+
     
