57d93a681cbc9d362ccb1c594a95d865b805113c
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-02-07 13:03:09 +0100

    resource builder for local testing

--- AWSInteraction/AWSResourceBuilder.py
+++ AWSInteraction/AWSResourceBuilder.py
@@ -1,50 +1,24 @@
+from typing import Any
 import boto3
 import os
 
-class AWSResourceBuilder:
+class AWSResourceBuilder():
     
-    
-    @staticmethod
-    def get_s3_client(sessionContext=''):
-        #if 'accesskey' in sessionContext: aws sso login --profile jacopo
+    def __init__(self) -> None:
+        self.profile = os.environ['AWS_PROFILE']
+        self.region = os.environ['REGION']
+        self.session = self._create_session()
 
-        is_local = os.getenv('LOCAL')
-        if is_local is None:
-            session = boto3.Session()
-        
+    def _create_session(self):
+        if os.getenv('ENV') == 'local':
+            return boto3.Session(profile_name=self.profile, region_name=self.region)
         else:
-            session = boto3.Session(profile_name='jacopoaws')
-
-
-        # session = boto3.Session(
-        #     profile_name='jacopo',
-        #     region_name='eu-south-1',)
-            
-        # session = boto3.Session(
-        #         aws_access_key_id=sessionContext['accesskey'],
-        #         aws_secret_access_key=sessionContext['secretkey'],
-        #         
-        #         aws_session_token=sessionContext.get('sessiontoken')
-        #     )
-        s3 = session.client('s3')
-        return s3
+            return boto3.Session()
     
-    @staticmethod
-    def get_secret_manager_client(sessionContext=''):
-        lambda_f = True
-        if lambda_f:
-            session = boto3.Session()
-        # session = boto3.Session(
-        #     profile_name='jacopo',
-        #     region_name='eu-south-1',)
-            
-        # session = boto3.Session(
-        #         aws_access_key_id=sessionContext['accesskey'],
-        #         aws_secret_access_key=sessionContext['secretkey'],
-        #         
-        #         aws_session_token=sessionContext.get('sessiontoken')
-        #     )
-        sm = session.client('secretsmanager')
-        return sm
+    def get_s3_client(self):
+        return self.session.client('s3')
     
+    def get_secret_manager_client(self):
+        return self.session.client(service_name='secretsmanager')
+
     
