c618e44eaeafec9a25329cc0130e05f1144c94f9
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-03-07 15:16:11 +0100

    sql error fix

--- extractors/custom_extractors/wamderivati/complexity_custom/setting/functions.py
+++ extractors/custom_extractors/wamderivati/complexity_custom/setting/functions.py
@@ -1,41 +1,4 @@
-import pyodbc
-import PyPDF2
 from datetime import datetime
-from dateutil import parser
-
-
-def extract_text_from_pdf(pdf_path):
-    text = ""
-
-    with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-
-        for page_number in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_number]
-            text += page.extract_text()
-    return text
-
-
-def insert_dataframe_into_table(database, table, input_df):
-    server = 'WAM-HAL9000'
-    conn_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};trusted_connection=yes"
-    conn = pyodbc.connect(conn_str)
-    cursor = conn.cursor()
-    try:
-        columns = input_df.columns.tolist()
-        placeholders = input_df.loc[0, :].values.flatten().tolist()
-        placeholders = ['NULL' if val is None else val for val in placeholders]
-        query = f"INSERT INTO {database}.{table} ({', '.join(columns)}) VALUES ({str(placeholders)[1:-1]})"
-        query = query.replace("'NULL'", "NULL")
-        cursor.execute(query)
-        conn.commit()
-        print("Inserimento completato con successo!")
-    except Exception as e:
-        conn.rollback()
-        print("Errore durante l'inserimento:", e)
-    finally:
-        cursor.close()
-        conn.close()
 
 
 def get_new_column_name(column_dict, column):
@@ -43,7 +6,6 @@ def get_new_column_name(column_dict, column):
         if column in possible_columns:
             return new_column
     return column
-
 
 def truncate(x):
     if isinstance(x, str):
