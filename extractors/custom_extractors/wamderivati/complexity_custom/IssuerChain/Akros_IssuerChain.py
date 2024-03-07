439401ad7683600a838252d488e401a0f08e209f
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-03-07 15:01:21 +0100

    minimal fix

--- extractors/custom_extractors/wamderivati/complexity_custom/IssuerChain/Akros_IssuerChain.py
+++ extractors/custom_extractors/wamderivati/complexity_custom/IssuerChain/Akros_IssuerChain.py
@@ -1,19 +1,19 @@
-import pandas as pd
-from langchain.chains import create_tagging_chain_pydantic
-from Derivati.preprocessing.functions import run_replace_string_function
-from Derivati.setting.pydantic_schema import PydanticSchema_akros, PydanticSchema
+# import pandas as pd
+# from langchain.chains import create_tagging_chain_pydantic
+# # from Derivati.preprocessing.functions import run_replace_string_function
+# # from Derivati.setting.pydantic_schema import PydanticSchema_akros, PydanticSchema
 
 
-def Akros_IssuerChain(input_kid, llm):
+# def Akros_IssuerChain(input_kid, llm):
 
-    input_kid = run_replace_string_function(documents=input_kid, preprocessing_dict_static=False, preprocessing_dict_regex=False, preprocessing_dict_regex_dynamic=False)
+#     input_kid = run_replace_string_function(documents=input_kid, preprocessing_dict_static=False, preprocessing_dict_regex=False, preprocessing_dict_regex_dynamic=False)
 
-    base_chain = create_tagging_chain_pydantic(PydanticSchema_akros, llm)
-    base_guesses = base_chain.run(input_kid)
-    base_guesses_dict = dict(base_guesses)
-    df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
+#     base_chain = create_tagging_chain_pydantic(PydanticSchema_akros, llm)
+#     base_guesses = base_chain.run(input_kid)
+#     base_guesses_dict = dict(base_guesses)
+#     df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
 
-    return df_guesses
+#     return df_guesses
 
 
 
--- extractors/custom_extractors/wamderivati/complexity_custom/IssuerChain/Unicredit_IssuerChain.py
+++ extractors/custom_extractors/wamderivati/complexity_custom/IssuerChain/Unicredit_IssuerChain.py
@@ -1,29 +1,29 @@
-import pandas as pd
-from langchain.chains import create_tagging_chain_pydantic
-from llm_applications.Derivati.preprocessing.functions import run_replace_string_function
-from llm_applications.Derivati.preprocessing.preprocessing_config import preprocessing_unicredit_dict, preprocessing_unicredit_cedola_dict
-from llm_applications.Derivati.setting.pydantic_schema import PydanticSchema_unicredit, PydanticSchema_unicredit_noLangChain
+# import pandas as pd
+# from langchain.chains import create_tagging_chain_pydantic
+# from llm_applications.Derivati.preprocessing.functions import run_replace_string_function
+# from llm_applications.Derivati.preprocessing.preprocessing_config import preprocessing_unicredit_dict, preprocessing_unicredit_cedola_dict
+# from llm_applications.Derivati.setting.pydantic_schema import PydanticSchema_unicredit, PydanticSchema_unicredit_noLangChain
 
 
-def Unicredit_IssuerChain(input_kid, llm):
+# def Unicredit_IssuerChain(input_kid, llm):
 
-    input_kid = run_replace_string_function(documents=input_kid, preprocessing_dict_static=preprocessing_unicredit_dict,
-                                            preprocessing_dict_regex=False,
-                                            preprocessing_dict_regex_dynamic=preprocessing_unicredit_cedola_dict)
+#     input_kid = run_replace_string_function(documents=input_kid, preprocessing_dict_static=preprocessing_unicredit_dict,
+#                                             preprocessing_dict_regex=False,
+#                                             preprocessing_dict_regex_dynamic=preprocessing_unicredit_cedola_dict)
 
-    base_chain = create_tagging_chain_pydantic(PydanticSchema_unicredit, llm)
-    base_guesses = base_chain.run(input_kid)
-    base_guesses_dict = dict(base_guesses)
-    df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
+#     base_chain = create_tagging_chain_pydantic(PydanticSchema_unicredit, llm)
+#     base_guesses = base_chain.run(input_kid)
+#     base_guesses_dict = dict(base_guesses)
+#     df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
 
-    return df_guesses
+#     return df_guesses
 
 
-def Unicredit_IssuerChain_noLangchain(input_kid, llm):
+# def Unicredit_IssuerChain_noLangchain(input_kid, llm):
 
-    base_chain = create_tagging_chain_pydantic(PydanticSchema_unicredit_noLangChain, llm)
-    base_guesses = base_chain.run(input_kid)
-    base_guesses_dict = dict(base_guesses)
-    df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
+#     base_chain = create_tagging_chain_pydantic(PydanticSchema_unicredit_noLangChain, llm)
+#     base_guesses = base_chain.run(input_kid)
+#     base_guesses_dict = dict(base_guesses)
+#     df_guesses = pd.DataFrame.from_dict([base_guesses_dict])
 
-    return df_guesses
+#     return df_guesses
