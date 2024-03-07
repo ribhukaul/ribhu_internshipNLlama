7860d76d955704fe5605281f5eb314d8e63645d5
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-03-07 16:33:11 +0100

    updated cost gpt-3.5-16k

--- extractors/general_extractors/config/cost_config.py
+++ extractors/general_extractors/config/cost_config.py
@@ -6,13 +6,13 @@ cost_per_token = {
     "input": {
         "gpt-4-turbo": 0.00001,
         "gpt-4": 0.000028,
-        "gpt-3.5-turbo-16k": 0.000003,
+        "gpt-3.5-turbo-16k": 0.0000005,
         "gpt-3.5-turbo": 0.0000014,
     },
     "output": {
         "gpt-4-turbo": 0.000028,
         "gpt-4": 0.000056,
-        "gpt-3.5-turbo-16k": 0.000004,
+        "gpt-3.5-turbo-16k": 0.0000014,
         "gpt-3.5-turbo": 0.0000019,
     },
     "azure": 0.01,
