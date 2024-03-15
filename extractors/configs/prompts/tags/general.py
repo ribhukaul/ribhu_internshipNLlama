0fbece9f782523e34b5d0458673663caba7265f0
Jacopo Morabito <jacopo.morabito@prometeia.com>
2024-03-15 18:45:36 +0100

    general tag config

---
+++ extractors/configs/prompts/tags/general.py
@@ -0,0 +1,9 @@
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class DocLanguage(BaseModel):
+    language: str = Field(..., description="language of the document", enum=["it", "en", "fr", "de", "es"])
+
+class IsFirstPage(BaseModel):
+    is_first_page: bool = Field(..., description="True if the page is the first one, False otherwise")
+
